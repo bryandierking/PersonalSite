8d7fb9fd68b4288a1dcb049c3125ecdbc833d5a1
Bryan Dierking <47314894+bryandierking@users.noreply.github.com>
2019-07-14 15:46:22 -0700

    Create GalleryEntry.js

---
+++ public/JSON/GalleryEntry.js
@@ -0,0 +1,31 @@
+/**
+ * Filename: galleryEntry.js
+ * Company: Touchdown Delivery, LLC
+ * Author: Darien Tsai
+ * Collaborators: none
+ * Date Created: 1/6/19
+ * Description: 
+ * stores TDD gallery image names
+ */
+
+ /* positions stored in format of:
+{name: .jpg}
+ */
+
+ //stores the gallery iamge names as JSON objects
+var image = [
+  {name: "TDD_8_Charger.jpg"},
+  {name: "TDD_8_ChargerPort.jpg"},
+  {name: "TDD_8_DroneTop.jpg"},
+  {name: "TDD_8_ChargerVents.jpg"},
+  {name: "TDD_8_DroneBattery.jpg"},
+  {name: "TDD_8_DroneBottom.jpg"},
+  {name: "TDD_8_DroneCarryOpen.jpg"},
+  {name: "TDD_8_DroneFront.jpg"},
+  {name: "TDD_8_DroneProfile.jpg"},
+  {name: "TDD_8_DroneProp.jpg"},
+  {name: "TDD_8_DronePropMotion.jpg"},
+  {name: "TDD_8_DroneSide.jpg"},
+  {name: "TDD_8_DroneTop.jpg"},
+  {name: "TDD_8_DroneVent.jpg"}
+]
